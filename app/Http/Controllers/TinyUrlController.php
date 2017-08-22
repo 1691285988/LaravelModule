32dacb270919729fbfa7de106a54b08516b1f4e7
Goular <zhaojt_cs@126.com>
2017-08-22 10:49:12 +0800

    提交内容

--- app/Http/Controllers/TinyUrlController.php
+++ app/Http/Controllers/TinyUrlController.php
@@ -39,34 +39,42 @@ class TinyUrlController extends Controller
         if ($validator->fails()) {
             return back()->withInput()->withErrors($validator);
         } else {
-            $host = "http://6du.in/";
-            $path = "?is_api=1&lurl=";
-            $method = "GET";
-            $headers = array();
-            $query = request('url');
-            $url = $host . $path . $query;
-            $curl = curl_init();
-            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
-            curl_setopt($curl, CURLOPT_URL, $url);
-            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-            curl_setopt($curl, CURLOPT_FAILONERROR, false);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            //curl_setopt($curl, CURLOPT_HEADER, true);
-            if (1 == strpos("$" . $host, "https://")) {
-                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-                curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-            }
-            $url = curl_exec($curl);
-            if ($url) {
-                $tinyUrl = new TinyUrl();
-                $tinyUrl->url = $query;
-                $tinyUrl->shortUrl = $url;
-                $tinyUrl->save();
-                $result = $url;
-                $orginal = $query;
-                redirect('/tinyurls/index', compact('result', 'orginal'));
+            //判断数据库是否存在url
+            $tinyUrl = TinyUrl::where('url', request('url'))->first();
+            if ($tinyUrl) {
+                $result = $tinyUrl->shortUrl;
+                $orginal = $tinyUrl->url;
+                return view('tinyurl.index', compact('result', 'orginal'));
             } else {
-                redirect('/tinyurls/index');
+                $host = "http://6du.in/";
+                $path = "?is_api=1&lurl=";
+                $method = "GET";
+                $headers = array();
+                $query = request('url');
+                $url = $host . $path . $query;
+                $curl = curl_init();
+                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+                curl_setopt($curl, CURLOPT_URL, $url);
+                curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+                curl_setopt($curl, CURLOPT_FAILONERROR, false);
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                //curl_setopt($curl, CURLOPT_HEADER, true);
+                if (1 == strpos("$" . $host, "https://")) {
+                    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+                    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+                }
+                $url = curl_exec($curl);
+                if ($url) {
+                    $tinyUrl = new TinyUrl();
+                    $tinyUrl->url = $query;
+                    $tinyUrl->shortUrl = $url;
+                    $tinyUrl->save();
+                    $result = $url;
+                    $orginal = $query;
+                    return view('tinyurl.index', compact('result', 'orginal'));
+                } else {
+                    return view('tinyurl.index');
+                }
             }
         }
     }
