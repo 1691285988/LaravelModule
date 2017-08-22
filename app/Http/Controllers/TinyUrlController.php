9f2ae000ada25ac028a07382274f0d1d45b9914d
Goular <zhaojt_cs@126.com>
2017-08-22 09:26:38 +0800

    提交内容

--- app/Http/Controllers/TinyUrlController.php
+++ app/Http/Controllers/TinyUrlController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+/**
+ * 短地址控制器
+ * Class TinyUrlController
+ * @package App\Http\Controllers
+ */
 class TinyUrlController extends Controller
 {
     /**
@@ -29,7 +34,7 @@ class TinyUrlController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -40,7 +45,7 @@ class TinyUrlController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -51,7 +56,7 @@ class TinyUrlController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -62,8 +67,8 @@ class TinyUrlController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -74,7 +79,7 @@ class TinyUrlController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
--- resources/views/tinyurl/index.blade.php
+++ resources/views/tinyurl/index.blade.php
@@ -1,18 +1,19 @@
 <!doctype html>
 <html lang="zh-CN">
 <head>
-    <META charset="utf-8" >
+    <META charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>短链生成器-by zexeo</title>
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
     <style type="text/css">
-        h1{
+        h1 {
             margin-top: 100px;
             margin-bottom: 40px;
             text-align: center;
             font-size: 50px;
         }
-        .col-lg-8{
+
+        .col-lg-8 {
             margin: 0 auto;
             float: none;
             text-align: center;
@@ -32,16 +33,23 @@
             </div><!-- /input-group -->
         </form>
         <br><br>
-        <h2>您的短链:<a href="#" target="_blank">url.dev/asde</a></h2>
-        <div class="alert alert-warning alert-dismissable">
-            请输入合法的URL地址！
-        </div>
-
+        <h2>您的短链:
+            <a href="#" target="_blank">
+                @if(isset($result))
+                    {{$result}}
+                @endif
+            </a>
+        </h2>
+        @if (count($errors) > 0)
+            <div class="alert alert-warning alert-dismissable">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
     </div><!-- /.col-lg-8 -->
-
-
-
-
 </div>
 </body>
 </html>
