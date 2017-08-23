c1b1f2cf042793457d3cd166dcc553d20a506f69
Goular <zhaojt_cs@126.com>
2017-08-23 09:09:20 +0800

    邮箱测试

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -14,13 +14,13 @@ class MailController extends Controller
     public function send()
     {
 
-//        $flag = \Mail::raw('你好，我是PHP程序360！', function ($message) {
-//            $to = 'zhaojt_cs@126.com';
-//            $message->to($to)->subject('纯文本信息邮件测试');
-//        });
-//        dd($flag);
+        $flag = \Mail::raw('你好，我是PHP程序360！', function ($message) {
+            $to = 'zhaojt_cs@126.com';
+            $message->to($to)->subject('纯文本信息邮件测试');
+        });
+        dd($flag);
 
-        $job = (new SendMail())->delay(Carbon::now()->addSeconds(30));
-        $this->dispatch($job);
+//        $job = (new SendMail())->delay(Carbon::now()->addSeconds(30));
+//        $this->dispatch($job);
     }
 }
