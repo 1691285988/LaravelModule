3db39d2efccf65c355990e09db211b14add81c66
Goular <zhaojt_cs@126.com>
2017-08-23 16:28:48 +0800

    内容提交

--- app/Http/Controllers/OauthController.php
+++ app/Http/Controllers/OauthController.php
@@ -14,7 +14,7 @@ class OauthController extends Controller
      */
     public function redirectToProvider()
     {
-        return \Socialite::driver('github')->redirect();
+        return Socialite::driver('github')->redirect();
     }
 
     /**
@@ -24,7 +24,7 @@ class OauthController extends Controller
      */
     public function handleProviderCallback()
     {
-        $user = \Socialite::driver('github')->user();
+        $user = Socialite::driver('github')->user();
 
         \Log::info('GitHub三方登录', $user);
     }
