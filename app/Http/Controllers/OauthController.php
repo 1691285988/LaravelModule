905dc2ff115d2c9b7ef2910e8aac74cbc39abe2a
Goular <zhaojt_cs@126.com>
2017-08-24 17:40:24 +0800

    正式提交使用Oauth2.0使用的库，非常简单和方便

--- app/Http/Controllers/OauthController.php
+++ app/Http/Controllers/OauthController.php
@@ -5,8 +5,33 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Socialite;
 
+/**
+ * OAuth2.0的例子示范
+ * Class OauthController
+ * @package App\Http\Controllers
+ *
+ * 我使用的不是Laravel官方的Socialite
+ * 而是使用安正超的Laravel-socialite
+ *
+ * 文档的地址(使用安正超的这个库的好处是，集成了国内重要网站的三方登陆:)
+ * https://github.com/overtrue/laravel-socialite
+ * https://github.com/overtrue/socialite
+ *
+ * Now we support the following sites:
+ *
+ * facebook, github, google, linkedin, weibo, qq, wechat, wechat_open, and douban.
+ *
+ * Each drive uses the same configuration keys: client_id, client_secret, redirect.
+ *
+ * 'wechat_open' => [
+ * 'client_id'     => 'your-app-id',
+ * 'client_secret' => ['your-component-appid', 'your-component-access-token'],
+ * 'redirect'      => 'http://localhost/socialite/callback.php',
+ * ]
+ */
 class OauthController extends Controller
 {
+
     /**
      * Redirect the user to the GitHub authentication page.
      *
