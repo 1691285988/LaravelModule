5bbcb001e95d38b5b9dba712044f791f72f1d1e3
Goular <zhaojt_cs@126.com>
2017-08-27 15:53:44 +0800

    提交内容

---
+++ app/Api/Controllers/LessonController.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Api\Controllers;
+
+class LessonController extends BaseController
+{
+    public function index(){
+        return 'v1';
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => 'v1'], function () {
-    Route::resource('lessons', '\App\Http\Controllers\LessonController');
+//Route::group(['prefix' => 'v1'], function () {
+//    Route::resource('lessons', '\App\Http\Controllers\LessonController');
+//});
+
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1', function ($api) {
+    $api->group(['namespace' => 'App\Api\Controllers'], function ($api) {
+        $api->get('lessons', 'LessonController@index');
+    });
 });
