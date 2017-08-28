343f4074df085f04bfb669025df1c5b7602101ae
Goular <zhaojt_cs@126.com>
2017-08-28 09:46:41 +0800

    提交内容

--- app/Api/Controllers/AuthController.php
+++ app/Api/Controllers/AuthController.php
@@ -26,6 +26,12 @@ class AuthController extends BaseController
         // grab credentials from the request
         $credentials = $request->only('email', 'password');
 
+        //使用别的名称来作为登录的识别内容
+//        $credentials = array(
+//            'user_mail' => request('user_mail'),
+//            'password' => request('user_password'),
+//        );
+
         try {
             // attempt to verify the credentials and create a token for the user
             if (!$token = JWTAuth::attempt($credentials)) {
